342470a4f33a6af54d8fae4dd85cff83834b4540
Soul Pro <70108490+ESinclair81@users.noreply.github.com>
2020-09-26 03:24:47 -0400

    laid out template for questions js

---
+++ Assets/JS/questions.js
@@ -0,0 +1,63 @@
+//These are the questions, answers to choose from, and correct answers
+
+var questions = [
+{
+    title:
+    choices: [ ]
+    answer:
+},
+
+{
+    title:
+    choices: [ ]
+    answer:
+},
+
+{
+    title:
+    choices: [ ]
+    answer:
+},
+
+{
+    title:
+    choices: [ ]
+    answer:
+},
+
+{
+    title:
+    choices: [ ]
+    answer:
+},
+
+{
+    title:
+    choices: [ ]
+    answer:
+},
+
+{
+    title:
+    choices: [ ]
+    answer:
+},
+
+{
+    title:
+    choices: [ ]
+    answer:
+},
+
+{
+    title:
+    choices: [ ]
+    answer:
+},
+
+{
+    title:
+    choices: [ ]
+    answer:
+}
+];
