ba1d71ff43898d95c052a076b3780e723db3ad8b
Soul Pro <70108490+ESinclair81@users.noreply.github.com>
2020-09-27 13:05:19 -0400

    Troubleshootiing timer and hide css

--- Assets/JS/logic.js
+++ Assets/JS/logic.js
@@ -1,3 +1,66 @@
+// This is the questions array
+
+var questions = [
+    {
+        title: "What does PERSISTENCE refer to in JavaScript terms?",
+        choices: ["A file path that cannot fail", "The act of performing a PERSE","A user value that stays saved throughout a webpage", "When a coder codes for over 24 hours straight" ],
+        answer: "A user value that stays saved throughout a webpage"
+    },
+    
+    {
+        title: "Which one of these HTML Drag and Drop API events signifies the drag event is over?",
+        choices: ["dragdone","dragend","draggone","drag-end"],
+        answer: "dragend"
+    },
+    
+    {
+        title: "What does the abbreviation JSON stand for?",
+        choices: ["Java Script Object Name","JavaScript Only Notes", "Java Script Object Node", "JavaScript Object Notation"],
+        answer: "JavaScript Object Notation"
+    },
+    
+    {
+        title: "Another way of describing a callback is:_____?",
+        choices: ["passing a function through an event", "a prompt box that takes data a site refers to later", "passing a function through another function", "a function that makes mobile phones ring" ],
+        answer: "passing a function through another function"
+    },
+    
+    {
+        title: "What suffix should be used for a DOM element?",
+        choices: [" .El", " .el", " .domEl", " .DOM" ],
+        answer: " .El"
+    },
+    
+    {
+        title: "Which of the below is an example of a variable declaration?",
+        choices: [" var.thingy"," 8 > 0i", " x is y", "var x = y" ],
+        answer: "var x = y"
+    },
+    
+    {
+        title: "Which of these symbols should be used to create a code block?",
+        choices: [" {}", "[]", "()", "]};" ],
+        answer: "{}"
+    },
+    
+    {
+        title: "i++ is an example of  _________?",
+        choices: ["a variable that adds things twice", "increment expression","increment object", "variable expression" ],
+        answer: "increment expression"
+    },
+    
+    {
+        title: "Which of the following do not refer to types of JavaScript data?",
+        choices: ["boolean, symbolic, number", "number, boolean, grid", "string, object, function", "number, boolean, string" ],
+        answer: "number, boolean, string"
+    },
+    
+    {
+        title: "What other language uses a similar comment syntax style to that used in JavaScript?",
+        choices: ["HTML","CSS","Pascal", "all of the above"],
+        answer: "CSS"
+    }
+    ]
 
 
 // Quiz Variables
